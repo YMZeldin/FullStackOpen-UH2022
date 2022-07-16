e739a482801f48dc8044821417fa15da298e2d3b
YMZeldin <105582186+YMZeldin@users.noreply.github.com>
2022-07-16 12:20:03 +0300

    Exercises 1.1 and 1.2 done

--- part-01/src/App.js
+++ part-01/src/App.js
@@ -1,38 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-/* To render a React component, start it's name with an uppercase letter */
-const Hello_c = (props) => {
+const Header_c = (props) => {
   return (
-    <div>
-      <p>Hello world!<br/>It is {Date().toString()}</p>
-      <p>Hello {props.name}, you are {props.age} years old</p>
-    </div>
+    <h1>{props.course}</h1>
   )
 }
 
-function App() {
-  const name = "Ann"
-  const age = 29
+const Content_c = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        console.log("Fist Hello_c")
-        <Hello_c name="Peter" age="34" />
-        <Hello_c name={name} age={age} />
-      </header>
+    <p>{props.part} - {props.exercises} exercises</p>
+  )
+}
+
+const Total_c = (props) => {
+  return (
+    <p>Total number of exercises - {props.n1 + props.n2 + props.n3}</p>
+  )
+}
+function App() {
+  const course = 'Half Stack application development'
+  const part01 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part02 = 'Using props to pass data'
+  const exercises2 = 7
+  const part03 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header_c course={course} />
+      <Content_c part={part01} exercises={exercises1}/>
+      <Content_c part={part02} exercises={exercises2}/>
+      <Content_c part={part03} exercises={exercises3}/>
+      <Total_c n1={exercises1} n2={exercises2} n3={exercises3}/>
     </div>
   );
 }
