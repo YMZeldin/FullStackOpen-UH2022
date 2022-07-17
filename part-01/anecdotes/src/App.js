6c03faaa991803a993fdbd8802cf07023c666d03
YMZeldin <105582186+YMZeldin@users.noreply.github.com>
2022-07-17 22:39:20 +0300

    Commit changes - not finished

--- part-01/anecdotes/src/App.js
+++ part-01/anecdotes/src/App.js
@@ -1,53 +1,53 @@
 // import logo from './logo.svg';
 // import './App.css';
+import {useState} from 'react'
 
-const Header = (props) => {
-  // console.log(props)
-  return <h1>{props.course}</h1>
-}
-
-const Content = (props) => {
-  // console.log(props.parts)
+const Button = ({ handleClick, text }) => {
   return (
-    <div>
-      <p>{props.parts[0].name} - {props.parts[0].exercises} exercises</p>
-      <p>{props.parts[1].name} - {props.parts[1].exercises} exercises</p>
-      <p>{props.parts[2].name} - {props.parts[2].exercises} exercises</p>  
-    </div>
+    <button onClick={handleClick}>{text}</button>
   )
 }
 
-const Total = (props) => {
-  return (
-    <p>Total number of exercises - {props.parts[0].exercises + 
-      props.parts[1].exercises + props.parts[2].exercises}</p>
-  )
-}
+const anecdotes = [
+  'If it hurts, do it more often.',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+  'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
+]
 
-function App() {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises:  7
-    },
-    {
-    name: 'State of a component',
-    exercises:  14
-    }
-  ]
-}
+const initialVotes = new Array(anecdotes.length).fill(0)
+
+const App = () => {
+
+  const [selected, setSelected] = useState(0)
+  const [allVotes, setVote] = useState(initialVotes)
+  
+  const max = anecdotes.length - 1
+
+  const nextAnecdote = () => {
+    const newSelection = Math.round(Math.random() * max)
+    // console.log('nextAnecdote.newSelection ', newSelection)
+    setSelected(newSelection)
+  }
+
+  const vote = () => {
+    const copy = [...allVotes]
+    copy[selected] += 1
+    setVote(copy)
+    console.log('allVotes', allVotes)
+  }
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <h1>anecdotes</h1>
+      <div>{anecdotes[selected]}</div>
+      <div>has {allVotes[selected]} votes</div>
+      <br />
+      <Button handleClick={vote} text='vote' />
+      <Button handleClick={nextAnecdote} text='next anecdote' />
     </div>
   )
 }
