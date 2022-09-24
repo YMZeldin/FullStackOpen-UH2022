d7d365c6e914afaa57c5785338f6c85c68c07171
Yury Zeldin <ymzeldin@outlook.com>
2022-09-24 15:24:15 +0300

    Ex 5.14 done

--- part-05/bloglist/src/components/Blog.test.js
+++ part-05/bloglist/src/components/Blog.test.js
@@ -1,27 +1,31 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
-//import userEvent from '@testing-library/user-event'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 describe('<Blog />', () => {
+  
+  const blog = {
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/',
+    likes: 2,
+    user: {
+      name: 'Yury Zeldin'
+    }
+  }
+
+  const user = {
+    token: '123456789ABCD',
+    username: 'zeldin',
+    name: 'Yury Zeldin'
+  }
+
+  const mockHandleRemoveBlog = jest.fn()
 
   // =============================================================================
   test('initial renders content', () => {
-    const blog = {
-      title: 'Canonical string reduction',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.cs.utexas.edu/',
-      likes: 2
-    }
-
-    const user = {
-      token: '123456789ABCD',
-      username: 'zeldin',
-      name: 'Yury Zeldin'
-    }
-
-    const mockHandleRemoveBlog = jest.fn()
 
     render(<Blog blog={blog} user={user} handleRemoveBlog={mockHandleRemoveBlog} />)
     // screen.debug()
@@ -35,6 +39,28 @@ describe('<Blog />', () => {
 
     const element3 = screen.queryByText('2')
     expect(element3).toBeNull()
+  })
+
+  // ===========================================================================
+  test('content after view button click', async () => {
+
+    render(<Blog blog={blog} user={user} handleRemoveBlog={mockHandleRemoveBlog} />)
+
+    const interactiveUser = userEvent.setup()
+    const button = screen.getByText('view')
+    await interactiveUser.click(button)
+
+    // screen.debug()
+
+    const element1 = screen.getByText('Canonical string reduction by Edsger W. Dijkstra')
+    expect(element1).toBeDefined()
+
+    // queryBy... returns null if no element found
+    const element2 = screen.queryByText('http://www.cs.utexas.edu/', {exact: false})
+    expect(element2).toBeDefined()
+
+    const element3 = screen.queryByText('2')
+    expect(element3).toBeDefined()
   })
 
 })
